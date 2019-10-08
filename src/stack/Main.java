ea10ae037ce70a964d251a21e2952581df61bbac
chaninkou <michaelkoulove@yahoo.com>
2019-10-07 18:19:21 -0700

    ok problem. Able to get the minimum of the stack.

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -3,13 +3,19 @@ package stack;
 public class Main {
 	public static void main(String[] args) {
 		 MinStack obj = new MinStack();
-		 obj.push(-2);
-		 obj.push(0);
-		 obj.push(-3);
-		 int param_3 = obj.top();
-		 int param_4 = obj.getMin();
 		 
-		 System.out.println("top: " + param_3);
-		 System.out.println("min: " + param_4);
+		 obj.push(-2);
+		 
+		 obj.push(0);
+		 
+		 obj.push(-3);
+		 
+		 System.out.println("min: " + obj.getMin());   
+		 
+		 obj.pop();
+		 
+		 System.out.println("top: " + obj.top()); 
+		 
+		 System.out.println("min: " + obj.getMin());  
 	}
 }
--- src/stack/MinStack.java
+++ src/stack/MinStack.java
@@ -6,27 +6,28 @@ public class MinStack {
     int min = Integer.MAX_VALUE;
 
     Stack<Integer> stack = new Stack<>();
-    
-    /** initialize your data structure here. */
-    public MinStack() {
-        
-    }
-    
+  
+    // Reason why we push twice is so we could keep track of min and second min
     public void push(int x) {
         // If the value is less than min, then push the min first and then push the value
         if(x <= min){
-            stack.push(min);
-            min = x;
+        	// Push the previous min
+        	stack.push(min);
+        	
+        	// Min will now equal to current min
+        	min = x;
         }
+        
+        // Push the current min
         stack.push(x);
     }
     
     public void pop() {
         // If pop is min, then pop again to get the second min.
-        if(stack.pop() == min){
-            min = stack.pop();
-            System.out.println(min);
-        }
+    	if(stack.pop() == min){
+    		// The min will be the second min now
+    		min = stack.pop();
+    	}
     }
     
     public int top() {
