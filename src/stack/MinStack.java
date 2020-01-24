67b5b87d1f4f686e2498ff6f9a983c445144c1ba
chaninkou <michaelkoulove@yahoo.com>
2020-01-23 19:19:47 -0800

    Ok review on stack, problem kinda useless,but great review on stack.

--- src/stack/MinStack.java
+++ src/stack/MinStack.java
@@ -11,19 +11,20 @@ public class MinStack {
     public void push(int x) {
     	// We want equal to because we need to know which one is the second min also
     	if(x <= min){
-    		// Push the second min
+    		// Push the second min, cause we want to know what is the second min once we pop the first min
     		stack.add(min);
     		
     		// Update the current min
     		min = x;
     	}
     	
-    	// Push the value no matter what
+    	// Finally push the value, even if its not min
     	stack.push(x);
     }
     
     // For pop(), we gotta make sure we update the min value if we popped the min value
     public void pop() {
+    	// stack.pop() inside if statement will occur no matter what, then update the current min by popping the second min that was used as a temp
     	if(stack.pop() == min){
     		min = stack.pop();
     	}
